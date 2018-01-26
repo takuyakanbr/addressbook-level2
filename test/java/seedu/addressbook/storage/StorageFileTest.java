8872cf8bb7f66593b675cc97894bc0cd36b47729
Daniel Teo <takuyakanbr@gmail.com>
2018-01-26 15:46:18 +0800

    Fix junit tests to use updated address format

--- test/java/seedu/addressbook/storage/StorageFileTest.java
+++ test/java/seedu/addressbook/storage/StorageFileTest.java
@@ -112,12 +112,12 @@ public class StorageFileTest {
         ab.addPerson(new Person(new Name("John Doe"),
                                 new Phone("98765432", false),
                                 new Email("johnd@gmail.com", false),
-                                new Address("John street, block 123, #01-01", false),
+                                new Address("51, John street, #01-01, 123456", false),
                                 new UniqueTagList(Collections.emptySet())));
         ab.addPerson(new Person(new Name("Betsy Crowe"),
                                 new Phone("1234567", true),
                                 new Email("betsycrowe@gmail.com", false),
-                                new Address("Newgate Prison", true),
+                                new Address("0, Newgate Prison, 0, 0", true),
                                 new UniqueTagList(new Tag("friend"), new Tag("criminal"))));
         return ab;
     }
